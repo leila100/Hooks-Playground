ceed08f3fb2c2858645bc252aac976fe43e196d2
Leila <nb.leila10@gmail.com>
2020-07-01 17:58:06 -0500

    Example of how to use useEffect to cleanup before reRendering and unmounting

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React, { useState, useEffect } from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0);
+function App() {
+  const [userText, setUserText] = useState("");
+
+  const handleUseKeyPress = (event) => {
+    const { key, keyCode } = event;
+    if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+      setUserText(`${userText}${key}`);
+    }
+  };
 
   useEffect(() => {
-    console.log(`I'm inside the useEffect hook, current count is ${count}`);
+    window.addEventListener("keydown", handleUseKeyPress);
     return () => {
-      console.log(`count: ${count} - I am used to remove anything that was setup above`);
+      window.removeEventListener("keydown", handleUseKeyPress);
     };
-  }, [count]);
-
-  const incrementHandler = () => setCount((prevCount) => prevCount + 1);
-  const decrementHandler = () => setCount((prevCount) => prevCount - 1);
-
-  return (
-    <div className='App'>
-      <button onClick={incrementHandler}>increment</button>
-      <button onClick={decrementHandler}>decrement</button>
-      <h1>{count}</h1>
-    </div>
-  );
-}
-
-function App() {
-  const [visible, setVisible] = useState(false);
+  });
   return (
     <div>
-      <button onClick={() => setVisible((prevCount) => !prevCount)}>Show/Hide</button>
-      {visible && <Counter />}
+      <h1>Feel free to type some text, it will show below</h1>
+      <blockquote>{userText}</blockquote>
     </div>
   );
 }
