ce3c2fd51612b8ea8410ec636808d39b2e7d308f
Leila <nb.leila10@gmail.com>
2020-07-01 16:08:05 -0500

    Add simple useState example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [activated, setActivated] = useState(false);
+  const buttonText = activated ? "Active" : "Inactive";
+
+  const clickHandler = () => {
+    setActivated(!activated);
+  };
   return (
     <div className='App'>
       <h1>Welcome to the course</h1>
+      <button onClick={clickHandler}>{buttonText}</button>
     </div>
   );
 }
